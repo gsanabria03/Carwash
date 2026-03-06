c9b8466b5ae8c9c862bdd898493c740b79abba73
gsanabria03 <142358070+gsanabria03@users.noreply.github.com>
2026-03-06 12:37:49 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-apiKey: "APIKEY",
-authDomain: "PROJECT.firebaseapp.com",
-projectId: "PROJECTID",
-storageBucket: "PROJECT.appspot.com",
-messagingSenderId: "XXXX",
-appId: "XXXX"
+apiKey: "AIzaSyAWtZcGuArCASuqBNXezPTf6tFmRZl0xeY",
+authDomain: "carwash-75ebc.firebaseapp.com",
+projectId: "carwash-75ebc",
+storageBucket: "carwash-75ebc.firebasestorage.app",
+messagingSenderId: "655463719693",
+appId: "1:655463719693:web:2591af251845ada7980644"
+
 };
