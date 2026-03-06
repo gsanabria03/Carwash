0938245c89c8a338178dd06306a2d8dc1774d1de
gsanabria03 <142358070+gsanabria03@users.noreply.github.com>
2026-03-06 12:26:10 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+apiKey: "APIKEY",
+authDomain: "PROJECT.firebaseapp.com",
+projectId: "PROJECTID",
+storageBucket: "PROJECT.appspot.com",
+messagingSenderId: "XXXX",
+appId: "XXXX"
+};
